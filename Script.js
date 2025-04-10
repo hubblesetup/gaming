586f497f4a2c9c968f40b35637e79b06e3b884dd
exofiex6 <jacetompsonex6@gmail.com>
2025-04-10 12:30:29 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -10,56 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-console.log("you shouldn't be seeing this");
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DOMContentLoaded event fired");
-});
-
-document.addEventListener("contextmenu", (event) => event.preventDefault());
-
-document.addEventListener("keydown", (event) => {
-    if (
-        event.key === "F12" ||
-        (event.ctrlKey && event.shiftKey && (event.key === "I" || event.key === "J" || event.key === "C")) ||
-        (event.ctrlKey && event.key === "U")
-    ) {
-        event.preventDefault();
-    }
-});
-
-if (window.location.href.startsWith("view-source:")) {
-    window.location.replace("https://www.google.com");
- 
-}
-
-let devtoolsOpen = false;
-const checkDevTools = () => {
-    const threshold = 160;
-    const widthThreshold = window.outerWidth - window.innerWidth > threshold;
-    const heightThreshold = window.outerHeight - window.innerHeight > threshold;
-    if (widthThreshold || heightThreshold) {
-        devtoolsOpen = true;
-        window.open('', '_self').close();
-        window.location.replace("https://www.google.com");
-    } else {
-        devtoolsOpen = false;
-    }
-};
-setInterval(checkDevTools, 1000);
-
-setInterval(() => {
-    console.clear();
-    console.log = function () {};
-    console.warn = function () {};
-    console.error = function () {};
-}, 100);
-
-if (window.location.href.startsWith("view-source:")) {
-        document.write(" < h1 > 500 Internal Server Error < /h1>");
-            document.write(" < p > The server encountered an internal error. < /p>");
-              throw new Error("Fake Server Error to Block Source Code Viewing");
-            }
 
 
 
